9d775d5284faae9fe51e17ecd0f955199c8e6589
David Lonyai <david.lonyai@gmail.com>
2023-01-19 20:53:23 +0100

    fix(npm): add npmrc

--- build_npm_package.ts
+++ build_npm_package.ts
@@ -36,3 +36,7 @@ await build({
 // post build steps
 Deno.copyFileSync("LICENSE", "npm/LICENSE");
 Deno.copyFileSync("README.md", "npm/README.md");
+Deno.writeTextFileSync(
+  "npm/.npmrc",
+  "//registry.npmjs.org/:_authToken=${NPM_TOKEN}",
+);
