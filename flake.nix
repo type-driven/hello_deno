8aeaf25c6aac66e5214e84faf08d5b534ebd593c
D <david.lonyai@gmail.com>
2023-10-02 18:52:39 +0200

    nix init

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A Nix-flake-based Deno development environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      overlays = [
+        (final: prev: rec {
+          deno = prev.deno;
+          just = prev.just;
+        })
+      ];
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit overlays system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [ deno just ];
+        };
+      });
+    };
+}
